79c57f909e6daee28bdd008120bfac7aea5b930f
Rubayth Haque <rubayth19@gmail.com>
2019-04-10 03:15:24 -0400

    created pages using fullPage.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import Particles from 'react-particles-js';
+import particleParam from './particleParam';
+import ReactFullpage from '@fullpage/react-fullpage';
 import Navigation from './components/Navigation/Navigation';
+import Home from './components/Home/Home';
 
 class App extends Component {
+
+  onLeave(origin, destination, direction) {
+    console.log("Leaving section " + origin.index);
+  }
+  afterLoad(origin, destination, direction) {
+    console.log("After load: " + destination.index);
+  }
   render() {
     return (
-      <Navigation></Navigation>
+      <ReactFullpage
+        anchors={["home", "about", "portfolio", "resume"]}
+        sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
+        scrollOverflow={true}
+        onLeave={this.onLeave.bind(this)}
+        afterLoad={this.afterLoad.bind(this)}
+        render={({ state, fullpageApi }) => {
+          return (
+            <div id="fullpage-wrapper">
+              <div className="section">
+                <Home/>
+                <button onClick={() => fullpageApi.moveSectionDown()}>
+                  Learn More
+                </button>
+              </div>
+              <div className="section">
+                <h3>About Me</h3>
+              </div>
+              <div className="section">
+                <div className="slide">
+                  <h3>Projects</h3>
+                </div>
+                <div className="slide">
+                  <h3>Slide 2.2</h3>
+                </div>
+                <div className="slide">
+                  <h3>Slide 2.3</h3>
+                </div>
+              </div>
+              <div className="section">
+                <h3>Resume</h3>
+              </div>
+            </div>
+          );
+        }}
+      />
     );
   }
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
-const Home = ({onRouteChange, isSignedIn}) => {
-    return {
-        
-    }
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import Particles from 'react-particles-js';
+import particleParam from '../../particleParam';
+const Home = () => {
+    return (
+        <div>
+            <Jumbotron>
+            
+            <h1>Rubayth Haque</h1>
+            <p>
+                This is a simple hero unit, a simple jumbotron-style component for calling
+                extra attention to featured content or information.
+            </p>
+            </Jumbotron>
+        </div>
+        )
 }
 
 export default Home;
