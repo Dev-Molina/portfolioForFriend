6ce2e74d5c03035920a7f79156b5cfbaa581fc5e
Rubayth Haque <rubayth@Rubayths-MacBook-Pro.local>
2019-04-11 18:48:36 -0400

    created states, animation

--- src/App.js
+++ src/App.js
@@ -4,34 +4,93 @@ import 'animate.css';
 import ReactFullpage from '@fullpage/react-fullpage';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Portfolio from './components/Portfolio/Portfolio';
+import Resume from './components/Resume/Resume';
 import {Button} from 'react-bootstrap';
 
+const state = {
+  home:false,
+  about:false,
+  portfolio:false,
+  resume:false
+}
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state=state;
+  }
+
+  onLeave = (origin, destination, direction) => {
+    switch(origin.index){
+      case 0: 
+        this.setState({home:false})
+        break;
+      case 1: 
+        this.setState({about:false})
+        break;
+      case 2: 
+        this.setState({portfolio:false})
+        break;
+      case 3: 
+        this.setState({resume:false})
+        break;
+      default:
+        break;
+    }
+    console.log("Leaving section " + origin.index);
+  }
+  
+  afterLoad = (origin, destination, direction) => {
+    //let loadedSection = this;
+    switch(destination.index){
+      case 0: 
+        this.setState({home:true})
+        break;
+      case 1: 
+        this.setState({about:true})
+        break;
+      case 2: 
+        this.setState({portfolio:true})
+        break;
+      case 3: 
+        this.setState({resume:true})
+        break;
+      default:
+        break;
+    }
+    console.log("After load: " + destination.index);
+  }
+
   render() {
     return (
       <div className = "app">
       <ReactFullpage
+        animateAnchor={false}
         navigation
         navigationTooltips={["Home", "About Me", "Portfolio", "Resume"]}
         scrollOverflow={true}
-      
+        onLeave={this.onLeave.bind(this)}
+        afterLoad={this.afterLoad.bind(this)}
         render={({ state, fullpageApi }) => {
           return (
-            <div id="fullpage-wrapper">
+            <ReactFullpage.Wrapper>
               <div className="section home">
-                <Home/>
+                <Home home={this.state.home}/>
                 <Button variant="outline-secondary" size="lg">Learn More</Button>
               </div>
               <div className="section">
-                <About/>
+                <About about={this.state.about}/>
               </div>
               <div className="section">
-                <h3>Projects</h3>
+                <Portfolio/>
+                <h3>Portfolio</h3>
               </div>
               <div className="section">
+                <Resume/>
                 <h3>Resume</h3>
               </div>
-            </div>
+            </ReactFullpage.Wrapper>
           );
         }}
       />
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './About.css';
-import { Jumbotron, Image } from 'react-bootstrap';
-import background from '../../assets/aboutBackground.PNG'
-const Home = () => {
-    return (
-        <div>
-            <img src={background} className='about' style={{zIndex:1}}/>
-            <div className='info' style={{zIndex:1000}}>
-            <p>I am a motivated new graduate in Computer Science and a passionate web developer/programmer with a relentless work ethic; I always aim to see things through. Problem-solving is my hobby with a genuine love for using cutting-edge technologies to build quality projects such as React with Redux! I have always been interested in web development and with a job in this field I want to improve my knowledge and my skills in this sector.</p>
+import 'animate.css';
+//import { Jumbotron } from 'react-bootstrap';
+
+
+const About = ({about}) => {
+    if(about){
+        return (
+            <div className='about'>
+                <p className='info animated fadeIn'>I am a motivated new graduate in Computer Science and a passionate web developer/programmer with a relentless work ethic; I always aim to see things through. Problem-solving is my hobby with a genuine love for using cutting-edge technologies to build quality projects such as React with Redux! I have always been interested in web development and with a job in this field I want to improve my knowledge and my skills in this sector.</p>
             </div>
-        </div>
-    )
+        )
+    }
+    else{
+        return(
+            <div className='about'>
+                <p className='info animated fadeOut'>I am a motivated new graduate in Computer Science and a passionate web developer/programmer with a relentless work ethic; I always aim to see things through. Problem-solving is my hobby with a genuine love for using cutting-edge technologies to build quality projects such as React with Redux! I have always been interested in web development and with a job in this field I want to improve my knowledge and my skills in this sector.</p>
+            </div>
+        )
+    }
 }
 
-
-export default Home;
+export default About;
